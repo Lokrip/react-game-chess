5fb114b0f2260f8adbdc835cf075fc1597c8b2b9
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-01-21 18:36:04 +0600

    game end

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import BoardComponent from './components/BoardComponent'
 import { Board } from './models/Board'
+import { Player } from './models/Player'
+import { Colors } from './models/Colors'
+import LostFigures from './components/LostFigures'
+import Timer from './components/Timer'
 
 function App() {
   const [board, setBoard] = useState(new Board())
-  const [whitePlayer]
-  console.log(board)
+
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
   useEffect(() => {
-    restart()
+    restart();
+    setCurrentPlayer(whitePlayer);
   }, [])
 
   function restart() {
@@ -19,9 +27,34 @@ function App() {
     setBoard(newBoard)
   }
 
+  function swapPlayer() {
+    setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+  }
+
   return (
     <div className="app">
-      <BoardComponent board={board} setBoard={setBoard} />
+      <Timer 
+        restart={restart}
+        currentPlayer={currentPlayer}
+      />
+      <BoardComponent 
+        board={board} 
+        setBoard={setBoard} 
+        currentPlayer={currentPlayer}
+        swapPlayer={swapPlayer}
+      />
+
+      <div className='lost-container'>
+        <LostFigures 
+          title="Black Figures"
+          figures={board.lostBlackFigures} 
+        />
+
+        <LostFigures 
+          title="White Figures"
+          figures={board.lostWhiteFigures} 
+        />      
+      </div>
     </div>
   )
 }
--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -2,21 +2,27 @@ import {FC, Fragment, useEffect, useState} from 'react';
 import { Board } from '../models/Board';
 import CellComponent from './CellComponent';
 import { Cell } from '../models/Cell';
+import { Player } from '../models/Player';
 
 interface BoardProps {
     board: Board;
     setBoard: (board: Board) => void;
+    currentPlayer: Player | null;
+    swapPlayer: () => void;
 }
 
-const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+const BoardComponent: FC<BoardProps> = ({board, setBoard, currentPlayer, swapPlayer}) => {
     const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
     function click(cell: Cell) {
         if(selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
             selectedCell.moveFigure(cell);
+            swapPlayer();
             setSelectedCell(null);
         } else {
-            setSelectedCell(cell);
+            if(cell.figure?.color === currentPlayer?.color) {
+                setSelectedCell(cell);
+            }
         }
     }
 
@@ -35,19 +41,22 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
     }
 
     return (
-        <div className='board'>
-            {board.cells.map((row, index) => 
-                <Fragment key={index}>
-                    {row.map((cell: any) => 
-                        <CellComponent
-                            click={click}
-                            cell={cell}
-                            key={cell.id}
-                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
-                        />
-                    )}
-                </Fragment>
-            )}
+        <div>
+            <h3 className='currentPlayer'>Current Player {currentPlayer?.color}</h3>
+            <div className='board'>
+                {board.cells.map((row, index) => 
+                    <Fragment key={index}>
+                        {row.map((cell: any) => 
+                            <CellComponent
+                                click={click}
+                                cell={cell}
+                                key={cell.id}
+                                selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                            />
+                        )}
+                    </Fragment>
+                )}
+            </div>
         </div>
     );
 };
